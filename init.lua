0aaec99963dadaec35a2df84d6e6ff6d4c6cbb3c
4Evergreen4 <eljohnson@frontier.com>
2014-03-18 20:59:44 -0400

    Add random table of quips, and bump version to 0.1.1

--- init.lua
+++ init.lua
@@ -1,8 +1,42 @@
 -----------------------------------------------------------------------------------------------
 local title	= "Death Messages"
-local version = "0.1"
+local version = "0.1.1"
 local mname	= "death_messages"
 -----------------------------------------------------------------------------------------------
+
+
+-- A table of quips for death messages
+
+local messages = {}
+
+-- Lava death messages
+messages.lava = {
+	" thought lava was cool.",
+	" melted into a ball of fire.",
+	" couldn't resist that warm glow of lava."
+}
+
+-- Drowning death messages
+messages.water = {
+	" ran out of air.",
+	" failed at swimming lessons.",
+	" tried to impersonate an anchor."
+}
+
+-- Burning death messages
+messages.fire = {
+	" burned to a crisp.",
+	" got a little too warm.",
+	" got too close to the camp fire."
+}
+
+-- Other death messages
+messages.other = {
+	" did something fatal.",
+	" died.",
+	" gave up on life."
+}
+
 
 minetest.register_on_dieplayer(function(player)
 	local player_name = player:get_player_name()
@@ -12,16 +46,16 @@ minetest.register_on_dieplayer(function(player)
 	-- Death by lava
 	local nodename = minetest.get_node(player:getpos()).name
 	if nodename == "default:lava_source" or nodename == "default:lava_flowing" then
-		minetest.chat_send_all(player_name .. " melted into a ball of fire.")
+		minetest.chat_send_all(player_name ..  messages.lava[math.random(1,#messages.lava)] )
 	-- Death by drowning
 	elseif nodename == "default:water_source" or nodename == "default:water_flowing" then
-		minetest.chat_send_all(player_name .. " ran out of air.")
+		minetest.chat_send_all(player_name ..  messages.water[math.random(1,#messages.water)] )
 	--Death by fire
 	elseif nodename == "fire:basic_flame" then
-		minetest.chat_send_all(player_name .. " burned up.")
+		minetest.chat_send_all(player_name ..  messages.fire[math.random(1,#messages.fire)] )
 	--Death by something else
 	else
-		minetest.chat_send_all(player_name .. " died.")
+		minetest.chat_send_all(player_name ..  messages.other[math.random(1,#messages.other)] )
 	end
 
 end)
